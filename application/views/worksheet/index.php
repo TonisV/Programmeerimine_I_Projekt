9dbeed56d65f4280101c1d21db9795b3c341e02d
TõnisV <tonisv@tlu.ee>
2017-05-14 23:24:19 +0300

    töölehe täiustused koos javascriptiga

--- application/views/worksheet/index.php
+++ application/views/worksheet/index.php
@@ -1,6 +1,6 @@
 <div class="container">
     <h2>Tööleht</h2>
-    <table>
+    <table class="work-table">
         <thead>
             <tr>
                 <th>Töö nr.</th>
@@ -18,20 +18,65 @@
             </tr>
         </thead>
         <tbody>
-            <tr>
+
+            <tr id="row-1" class="work-table-row">
                 <td>0001</td>
-                <td>Test Test</td>
-                <td>+372 0000 0000</td>
-                <td>test@test.ee</td>
-                <td>Sülearvuti Lenovo T420 + laadija</td>
-                <td>Ei lae, viirused, pahavara</td>
-                <td></td>
-                <td>65.00 €</td>
-                <td>2555</td>
+                <td><input name="client_name" type="text" value="Test Test" disabled /></td>
+                <td><input name="client_phone" type="text" value="580 00000" disabled /></td>
+                <td><input name="client_email" type="text" value="test@test.ee" disabled /></td>
+                <td><input name="job_equipment" type="text" value="Sülearvuti Lenovo T420 + laadija" disabled /></td>
+                <td><input name="job_problem" type="text" value="Ei lae viirused, pahavara" disabled /></td>
+                <td><input name="job_work" type="text" value="Tehtud hooldus" disabled /></td>
+                <td><input name="job_price" type="number" value="65.00" disabled /></td>
+                <td><input name="job_invoice" type="number" value="2555" disabled /></td>
                 <td>04.05.2017 10:50</td>
                 <td> - </td>
                 <td>
-                    <select>
+                    <select name="job_status">
+                        <option value="1">Vastuvõetud</option>
+                        <option value="2">Ootel</option>
+                        <option value="3">Teavitatud</option>
+                        <option value="4">Makstud ja üleantud</option>
+                    </select>
+                </td>
+            </tr>
+
+            <tr id="row-2" class="work-table-row">
+                <td>0002</td>
+                <td><input name="client_name" type="text" value="Test Test" disabled /></td>
+                <td><input name="client_phone" type="text" value="580 00000" disabled /></td>
+                <td><input name="client_email" type="text" value="test@test.ee" disabled /></td>
+                <td><input name="job_equipment" type="text" value="Sülearvuti Lenovo T420 + laadija" disabled /></td>
+                <td><input name="job_problem" type="text" value="Ei lae viirused, pahavara" disabled /></td>
+                <td><input name="job_work" type="text" value="Tehtud hooldus" disabled /></td>
+                <td><input name="job_price" type="number" value="65.00" disabled /></td>
+                <td><input name="job_invoice" type="number" value="2555" disabled /></td>
+                <td>04.05.2017 10:50</td>
+                <td> - </td>
+                <td>
+                    <select name="job_status">
+                        <option value="1">Vastuvõetud</option>
+                        <option value="2">Ootel</option>
+                        <option value="3">Teavitatud</option>
+                        <option value="4">Makstud ja üleantud</option>
+                    </select>
+                </td>
+            </tr>
+
+            <tr id="row-3" class="work-table-row">
+                <td>0003</td>
+                <td><input name="client_name" type="text" value="Test Test" disabled /></td>
+                <td><input name="client_phone" type="text" value="580 00000" disabled /></td>
+                <td><input name="client_email" type="text" value="test@test.ee" disabled /></td>
+                <td><input name="job_equipment" type="text" value="Sülearvuti Lenovo T420 + laadija" disabled /></td>
+                <td><input name="job_problem" type="text" value="Ei lae viirused, pahavara" disabled /></td>
+                <td><input name="job_work" type="text" value="Tehtud hooldus" disabled /></td>
+                <td><input name="job_price" type="number" value="65.00" disabled /></td>
+                <td><input name="job_invoice" type="number" value="2555" disabled /></td>
+                <td>04.05.2017 10:50</td>
+                <td> - </td>
+                <td>
+                    <select name="job_status">
                         <option value="1">Vastuvõetud</option>
                         <option value="2">Ootel</option>
                         <option value="3">Teavitatud</option>
@@ -42,19 +87,24 @@
 
         </tbody>
         <tfoot>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
+            <tr id="last_row">
+                <td>0004</td>
+                <td><input name="client_name"  type="text" placeholder=" - " /></td>
+                <td><input name="client_phone"  type="text" placeholder=" - " /></td>
+                <td><input name="client_email"  type="text" placeholder=" - " /></td>
+                <td><input name="job_equipment"  type="text" placeholder=" - " /></td>
+                <td><input name="job_problem"  type="text" placeholder=" - " /></td>
+                <td><input name="job_work"  type="text" placeholder=" - " /></td>
+                <td><input name="job_price"  type="number" placeholder="00.00" /></td>
+                <td><input name="job_invoice"  type="number" placeholder="0" /></td>
+                <td> - </td>
+                <td> - </td>
+                <td>
+                    <select name="job_status" id="last_row_select">
+                        <option value="1"> - </option>
+                        <option value="2">Vastuvõetud</option>
+                    </select>
+                </td>
             </tr>
         </tfoot>
     </table>
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,61 @@
-/* TV */
+/*
+*
+* TV Worksheet - Scripts
+*
+*/
+
+$(function() {
+
+});
+
+
+/* Double click events in rows */
+$('.work-table tbody td').dblclick(function(event){
+    event.preventDefault();
+
+    var $cellInput = $(this).children('input');
+
+    var rowId      = $(this).parent().attr('id');
+    var cellName   = $cellInput.attr('name');
+    var cellData   = $cellInput.val();
+
+    // Enable element
+    $cellInput.prop("disabled", false);
+
+    // After element loses focus Save new value and disable element
+    $cellInput.focusout(function () {
+        if (cellData !== $cellInput.val()) {
+            saveCell(rowId, cellName, $cellInput.val());
+        }
+        $cellInput.prop("disabled", true);
+    });
+
+});
+
+/* Last row select event */
+$('#last_row_select').change(function(){
+
+    var selectedValue = $(this).val();
+    var cells = $('#lastrow').find(':input').serializeArray();
+
+
+
+    // Enable element
+    if (selectedValue === 2) {
+        saveNewRow(cells);
+    }
+
+});
+
+
+function saveCell(rowId, cellName, data) {
+    console.log('saved - ' + rowId + ', ' + cellName + ', ' + data);
+}
+
+function saveNewRow(cells) {
+    console.log('saved - ' + cells);
+}
+
+function updateRow(rowId, cells) {
+    console.log('saved - ' + rowId + ', ' + cells);
+}
